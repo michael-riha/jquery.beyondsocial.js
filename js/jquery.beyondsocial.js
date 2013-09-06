7a39934abb88fbea40117d736f07c43ed26b10f4
Bernhard Prochazka <bernhard.prochazka@beyondthestatic.com>
2013-09-06 15:40:16 +0200

    example finished

--- js/jquery.beyondsocial.js
+++ js/jquery.beyondsocial.js
@@ -1,31 +1,34 @@
 //AJAX request
 (function($) {
 
-    $.beyondsocial = function(element, options) {
+	$.beyondsocial = function(element, options) {
 
-        var defaults = {
-            networks: [{name: 'tumblr', id: 'beyond-t-s'}, {name: 'tumblr', id: 'skindy'}, {name: 'facebook', id: '323476337711540'}],
-	    	maxResults: 4
-        }
+		var defaults = {
+			networks: [{name: 'tumblr', id: 'beyond-t-s'}, {name: 'tumblr', id: 'skindy'}, {name: 'facebook', id: '323476337711540'}],
+		    	maxResults: 4,
+			template: "mustache/rss.mustache",
+			bulkload: true
+		}
 
-        var plugin = this;
+		var plugin = this;
 
-        plugin.settings = {}
-		var result = [];
+		plugin.settings = {}
+			var result = [];
+			var networkResult = [];
 
-        var $element = $(element),
-             element = element;
+		var $element = $(element),
+		     element = element;
 
-        plugin.init = function() {
-            plugin.settings = $.extend({}, defaults, options);
-            plugin.bsreplaceurl();
-	    	//console.log($element);
-        }
+		plugin.init = function() {
+		    plugin.settings = $.extend({}, defaults, options);
+		    plugin.bsreplaceurl();
+		    	//console.log($element);
+		}
 
-        plugin.bsreplaceurl = function () {
+		plugin.bsreplaceurl = function () {
 			var processList = new Array();
 			var parserhelp = new Array();
-		
+	
 
 			for (var i = 0; i < plugin.settings.networks.length; i++) {
 				var nw = helpers.networkDefs[plugin.settings.networks[i].name]; //get the definitions of the current feed (definitions are at the end of the code)
@@ -57,10 +60,10 @@
 		}
 		//end of bsreplaceurl
 		
-        var bsajaxreq = function(reqUrl) {
-         	return jQuery.ajax({type: 'GET', dataType: 'jsonp', url:reqUrl, success: function(data){} });
-        }
-        
+		var bsajaxreq = function(reqUrl) {
+		 	return jQuery.ajax({type: 'GET', dataType: 'jsonp', url:reqUrl, success: function(data){} });
+		}
+		
 		//strips tags from content
 		var bsstrip = function(html) {
 			var tmp = document.createElement("DIV");
@@ -70,11 +73,10 @@
 
 		var extractSrc = function(str, object) {
 			var src;
-			//console.log($("img", $(str)).attr('src'));
 			src = $("img", $(str)).attr('src');
 			return src;
 		}
-		
+	
 		var bsSortByKey = function(array, key) { //sorts the array by the given keyfield
 			return array.sort(function(a, b) {
 				var x = a[key]; 
@@ -83,11 +85,12 @@
 		}
 
 		//render function + callback (mustache for testing, change to general render function later)
-		var bsrenderMustache = function(data, templateuri, callback) {
+		var bsbulkrenderMustache = function(data, templateuri, callback) {
 			$.ajax({
 				type: 'GET',
 				url:  templateuri,
 				success:function(template, textStatus, XMLHttpRequest){
+					//console.log(data);
 					callback(Mustache.render(template, data));
 				},
 
@@ -96,12 +99,79 @@
 				}
 			});
 		}
-		
-		var bsmustachecallback = function(html) {
+		var bsrenderMustache = function(data, templateuri, callback) {
+			var html;
+			$.ajax({
+				type: 'GET',
+				async: false,
+				url:  templateuri,
+				success:function(template, textStatus, XMLHttpRequest){
+					//console.log(data);
+					html = Mustache.render(template, data);
+				},
+
+				error: function(MLHttpRequest, textStatus, errorThrown){
+					console.log(errorThrown);
+				}
+			});
+			return html;
+		}
+
+		var bsbulkcallback = function(html) {
 			//console.log(html);
 			$element.html(html);
 		}
-
+		var bsgetfinaldata = function(entries, parserhelp, n) {
+			var retarr = new Array();
+			for(var j = 0; j<entries.length; j++) {
+				var helperA = new Array();
+				var entrycontent;
+				if(typeof(parserhelp[n].contentSel) == "object") {
+					var k = 0;
+					for(; !eval("entries[j]"+parserhelp[n].contentSel[k]); k++);
+					entrycontent = eval("entries[j]"+parserhelp[n].contentSel[k]);
+				
+				}
+				else{
+					entrycontent = eval("entries[j]"+parserhelp[n].contentSel);			//content with tags
+				}
+				//console.log(entries[j]);
+				helperA["title"] = eval("entries[j]"+parserhelp[n].titleSel);			//title
+				//convert publishdate to unix timestamp & convert from milliseconds to seconds
+				if( typeof (helperA["pubdate"] = eval("entries[j]"+parserhelp[n].dateSel)) != "number") {
+					helperA["pubdate"] = Date.parse(helperA["pubdate"]) / 1000;		
+				}
+				helperA["social"] = parserhelp[n].name;						//name of the social platform
+				helperA["link"] = eval("entries[j]"+parserhelp[n].linkSel);			//link to the post
+				//gets the first image. from the content or other arrayitems if there is no image get a placeholder
+				
+				if(helperA["social"] == "instagram") {
+					entryimg = extractSrc(eval("entries[j]"+parserhelp[n].imgSel))
+					helperA["image"] = entryimg;
+				}
+				else if(parserhelp[n].imgSel || parserhelp[n].vidSel) {
+					if(entryimg = eval("entries[j]"+parserhelp[n].imgSel)){
+						helperA["image"] = entryimg;
+					}
+					else if(entryimg = eval("entries[j]"+parserhelp[n].vidSel)) {
+						helperA["image"] = "http://img.youtube.com/vi/"+ entryimg.substring(16)+"/0.jpg";
+					}
+					else {
+						helperA["image"] = "http://placehold.it/100x100&text='No Image'";
+					}
+			}
+				else if(entryimg = extractSrc(entrycontent)) { 
+					helperA["image"] = entryimg;
+				}
+				else {
+					helperA["image"] = "http://placehold.it/100x100&text='No Image'";
+				}
+					//console.log(parserhelp[0]);
+				helperA["content"] = bsstrip(entrycontent); 					// strips each feedcontent from its tags
+				retarr.push(helperA);
+			}
+			return retarr;
+		}
 		//makes a new array to unify the different feeds, and makes them ready for rendering
 		var bsunify = function($, processList, parserhelp) {
 
@@ -112,115 +182,53 @@
 					/*console.log(JSON.stringify(arguments));
 					test = arguments[1][0];
 					console.log(test.responseJSON);*/
-					
+				
 				//}
+				var retarray = new Array();
 				if(datacount == 1)					//if there is only 1 feed to go through, arguments is only a one dimensional aray
 				{
 					entries = eval("arguments[0]"+parserhelp[0].resultsSel);		//the variable "arguments" holds the data
-
-					for(var j = 0; j<entries.length; j++) {
-						var helperA = new Array();
-						var entrycontent;
-						if(typeof(parserhelp[0].contentSel) == "object") {
-							var i = 0;
-							for(; !eval("entries[j]"+parserhelp[0].contentSel[i]); i++);
-							entrycontent = eval("entries[j]"+parserhelp[0].contentSel[i]);
-							
-						}
-						else{
-							entrycontent = eval("entries[j]"+parserhelp[0].contentSel);			//content with tags
-						}
-						console.log(entries[j]);
-						helperA["title"] = eval("entries[j]"+parserhelp[0].titleSel);			//title
-
-						//convert publishdate to unix timestamp & convert from milliseconds to seconds
-						if( typeof (helperA["pubdate"] = eval("entries[j]"+parserhelp[0].dateSel)) != "number") {
-							helperA["pubdate"] = Date.parse(helperA["pubdate"]) / 1000;		
-						}
-						helperA["social"] = parserhelp[0].name;						//name of the social platform
-						helperA["link"] = eval("entries[j]"+parserhelp[0].linkSel);			//link to the post
-
-						//gets the first image. from the content or other arrayitems if there is no image get a placeholder
-						//may need to be changed to a "switch"-command
-						if(entryimg = extractSrc(entrycontent)) { 
-							helperA["image"] = entryimg;
-						}
-						else if(entryimg = eval("entries[j]"+parserhelp[0].imgSel)) {
-							helperA["image"] = entryimg;
-						}
-						else if(entryimg = eval("entries[j]"+parserhelp[0].vidSel)) {
-							helperA["image"] = "http://img.youtube.com/vi/"+ entryimg.substring(16)+"/0.jpg";
-						}
-						else {
-							helperA["image"] = "http://placehold.it/100x100&text='No Image'";
-						}
-						helperA["content"] = bsstrip(entrycontent); 					// strips each feedcontent from its tags
-						
-						result.push(helperA);
-					}
+					//console.log(entries);
+					networkResult = bsgetfinaldata(entries, parserhelp, 0);
 				}
 				else if (datacount > 1) {									//for the two-dimensional "arguments" array
-
+					var renderedhtml = "";
 					//goes through each entry of every feed to extract the information and put it into another array
-					for(var i = 1; i < arguments.length-1; i++) { 
+					for(var i = 0; i < arguments.length; i++) { 
 						//parserhelp[i].mySelector gives you the association names for the various feeds -> search for "networkDefs" in this File for the listing
 						entries = eval("arguments[i][0]"+parserhelp[i].resultsSel);		//the variable "arguments" holds the data
 						//console.log(entries);
-						for(var j = 0; j<entries.length; j++) {
-							var helperA = new Array();
-							var entrycontent;			//content with tags
-							if(typeof(parserhelp[0].contentSel) == "object") {
-								var k = 0;
-								for(; !eval("entries[j]"+parserhelp[0].contentSel[k]); k++);
-								entrycontent = eval("entries[j]"+parserhelp[0].contentSel[k]);			//content with tags
-							
-							}
-							else{
-								entrycontent = eval("entries[j]"+parserhelp[0].contentSel);			//content with tags
-							}
-							helperA["title"] = eval("entries[j]"+parserhelp[i].titleSel);			//title
-
-							//convert publishdate to unix timestamp & convert from milliseconds to seconds
-							if( typeof (helperA["pubdate"] = eval("entries[j]"+parserhelp[i].dateSel)) != "number") {
-								helperA["pubdate"] = Date.parse(helperA["pubdate"]) / 1000;		
-							}
-							helperA["social"] = parserhelp[i].name;						//name of the social platform
-							helperA["link"] = eval("entries[j]"+parserhelp[i].linkSel);			//link to the post
-
-							//gets the first image. if there is no image get a placeholder
-							if(entryimg = extractSrc(entrycontent)) { 
-								helperA["image"] = entryimg;
-							}
-							else if(entryimg = eval("entries[j]"+parserhelp[0].imgSel)) {
-								helperA["image"] = entryimg;
-							}
-							else if(entryimg = eval("entries[j]"+parserhelp[0].vidSel)) {
-								helperA["image"] = "http://img.youtube.com/vi/"+ entryimg.substring(16)+"/0.jpg";
-							}
-							else {
-								helperA["image"] = "http://placehold.it/100x100&text='No Image'";
-							}
-
-							//console.log(helperA["title"]);
-							//console.log(entrycontent);
-							helperA["content"] = bsstrip(entrycontent); 					// strips each feedcontent from its tags
-							result.push(helperA);
-						}
+						result = bsgetfinaldata(entries, parserhelp, i);
 						//console.log(result);
+						if(!plugin.settings.bulkload) {
+							networkResult.push(result);
+							if (plugin.settings.networks[i].template)
+								renderedhtml += bsrenderMustache(result, plugin.settings.networks[i].template);
+							else
+								renderedhtml += bsrenderMustache(result, plugin.settings.template);
+						}
+						else {
+							for(var x=0; x<result.length; x++)
+								networkResult.push(result[x]);
+						}
 					}
+					
 				}
-				result = bsSortByKey(result, "pubdate");			//result is now sorted by Date with the fields: content, image, link, pubdat, social, title;
-				//console.log(result);
-				bsrenderMustache(result, 'mustache/rss.mustache',bsmustachecallback);
+				if(plugin.settings.bulkload || datacount == 1) {
+						networkResult = bsSortByKey(networkResult, "pubdate");			//result is now sorted by Date with the fields: content, image, link, pubdat, social, title;
+						//console.log(result);
+						bsbulkrenderMustache(networkResult, plugin.settings.template,bsbulkcallback);
+				}
+				else {
+					bsbulkcallback(renderedhtml);
+				}
 			}/*)(datacount, processList)*/);
 		}
 
-        plugin.init();
-
-    }
+    	plugin.init();
+	}
 
     $.fn.beyondsocial = function(options) {
-
         return $(this).each(function() {
             if (undefined == $(this).data('beyondsocial')) {
                 var plugin = new $.beyondsocial(this, options);
@@ -244,6 +252,7 @@
 					name: "facebook",
 					resultsSel: ".responseData.feed.entries",
 					imgSel: "",
+					vidSel: "",
 					titleSel:".title",
 					contentSel:".content",
 					dateSel: ".publishedDate",
@@ -269,11 +278,27 @@
 				googleapi: true,
 				parser:{
 				    	name: "pinterest",
-					resultsSel: "",
+					resultsSel: ".responseData.feed.entries",
 					imgSel: "",
-					titleSel:"",
-					contentSel:"",
-					linkSel: ""
+					vidSel: "",
+					titleSel:".title",
+					contentSel:".content",
+					dateSel: ".publishedDate",
+					linkSel: ".link"
+				}
+			},
+			instagram:{
+				url:'http://ink361.com/feed/user/[id]',
+				googleapi: true,
+				parser:{
+				    	name: "instagram",
+					resultsSel: ".responseData.feed.entries",
+					imgSel: ".content",
+					vidSel: "",
+					titleSel:".notitle",
+					contentSel:".title",
+					dateSel: ".publishedDate",
+					linkSel: ".link"
 				}
 			}
 			/*facebook:{url:'http://graph.facebook.com/[id]/photos?limit=[num]',img:'',dataType:'json',
